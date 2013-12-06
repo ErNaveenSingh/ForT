1b06b9a1e0af67be6d8691caae731e55fcae4ab2
ErNaveenSingh <er.nav.singh@gmail.com>
2013-12-06 09:39:27 +0530

    Added Comments

--- GetImDetails.java
+++ GetImDetails.java
@@ -1,5 +1,7 @@
 private void getImDetailsFor(String phoneNumber)
     {
+        //First step is to get the contact Id of the phone number
+        //Create URI for it
     	Uri uri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
     	ContentResolver cr = getContentResolver();
         Cursor cur = cr.query(
@@ -13,12 +15,20 @@ private void getImDetailsFor(String phoneNumber)
        	 	id = cur.getString(cur.getColumnIndex(PhoneLookup._ID));
        	 	Log.d("MYTAG", phoneNumber + ", id: " + id );
         }
+        
+        //Data.CONTENT_URI is the master URI to get all contact data
+        //Send the Im.CONTACT_ID and Data.MIMETYPE
         Cursor pCur = cr.query(
        		 Data.CONTENT_URI, 
        		 null, 
        		 ContactsContract.CommonDataKinds.Im.CONTACT_ID +" = ? and "+ContactsContract.Data.MIMETYPE+" = ?",
        		 new String[]{id, ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE}, 
        		 null);
+       		 
+        //Check this page :http://developer.android.com/reference/android/provider/ContactsContract.CommonDataKinds.Im.html#PROTOCOL
+        //Here you will find Contant Values for dataType below
+        // Eg. PROTOCOL_GOOGLE_TALK = 5
+        //PROTOCOL_SKYPE = 3
         while (pCur.moveToNext	()) {
         	String dataType = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Im.PROTOCOL));
         	String data = pCur.getString(pCur.getColumnIndex(ContactsContract.Data.DATA1));
